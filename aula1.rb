9cbf8736ed1d5d11d7e2f52f27f00c05805a618c
DaniloSalvato <danilo_salvato@hotmail.com>
2023-07-10 21:03:28 -0300

    basic

---
+++ aula1.rb
@@ -0,0 +1,70 @@
+#ex 1 
+nome = 'Danilo'
+idade = 23
+
+puts "Olá, meu nome é #{nome} e tenho #{idade} anos."
+
+
+#ex2
+x = 5
+
+puts "O dobro de #{x} é #{x * 2}"
+
+#ex3
+x = 5
+y = 4
+puts "A soma é #{x + y}"
+
+#ex4
+x = 5
+y = 4
+puts "A multiplicação é #{x * y}"
+
+#ex5
+x = 5
+y = 4
+puts "A subtração é #{x - y}"
+
+#ex6
+x = 5
+y = 4
+puts "A divisão é #{x / y}"
+
+#ex7
+x = 5
+puts "O quadrado é #{x**2}"
+
+#ex8
+x = 5
+y = 4
+puts "O resto é #{x % y}"
+
+#ex9    
+x = 5
+puts "Convertendo int para string #{x.to_s.class}"
+
+#ex10
+x = '10'
+puts "Convertendo string para int #{x.to_i.class}"
+
+#ex11   
+x = 15.2
+puts "Convertendo float para string #{x.to_s.class}"
+
+#ex12  
+x = '15'
+puts "Convertendo string para float #{x.to_f.class}"
+
+
+#teste
+
+x = 3.14 / 2
+puts x.class
+
+
+x = 1 + '1'
+y = 1 - '1'
+
+puts x 
+puts y
+
